7637e29de3f35844907e4e8e2d0bcce541ae5f19
liujinxin <724959554@qq.com>
2019-11-06 21:23:46 +0800

    2019/11/6 2th

--- spider/baiduwenku.py
+++ spider/baiduwenku.py
@@ -1,3 +1,9 @@
 import re
 import requests
 
+url = 'https://wenku.baidu.com/search?'
+session = requests.session()
+r = session.get(url)
+print(r.text)
+
+
--- spider/spiderl练习/yamaxun.py
+++ spider/spiderl练习/yamaxun.py
@@ -5,6 +5,7 @@ try:
 # print(res.status_code)
 # print(res.encoding)
 # print(res.apparent_encoding)
+    res.raise_for_status()
     res.encoding = res.apparent_encoding
     print(res.text)
 except:
