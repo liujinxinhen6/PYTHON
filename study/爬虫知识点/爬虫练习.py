33dacc40a5d698192d534d203fdda55c8294b9ca
liujinxin <724959554@qq.com>
2019-11-10 20:36:00 +0800

    2019/11/10

--- study/爬虫知识点/爬虫练习.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = 'https://item.jd.com/100006808184.html#none'
-try:
-	r = requests.get(url)
-	r.raise_for_status()
-	r.encoding = r.apparent_encoding
-	print(r.text[:1000])
-except:
-	print('抓取失败')
