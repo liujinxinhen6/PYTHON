6f7d3e97e8e28f69d57db6b9dd06c4b16186b3a9
liujinxin <724959554@qq.com>
2019-11-26 23:02:17 +0800

    2019/11/26

--- spider/喜马拉雅音频下载/喜马拉雅音频.py
+++ spider/喜马拉雅音频下载/喜马拉雅音频.py
@@ -6,7 +6,7 @@
 # @File    : 喜马拉雅音频.py
 # @Function: 喜马拉雅音频
 import requests
-from bs4 import BeautifulSoup
+import re
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
 }
@@ -18,9 +18,36 @@ def download(medium_name,medium_url):
 
 
 def getmessage():
-    response = requests.get('https://www.ximalaya.com/youshengshu/10587045/', headers = headers)
-    response.encoding = response.apparent_encoding
-    soup = BeautifulSoup(response.text,'html.parser')
-    print(soup.prettify())
+    trackId = []
+    title = []
+    for i in range(0,28):
+        page = i+1
+        response = requests.get('https://www.ximalaya.com/youshengshu/10587045/p' + str(page), headers = headers)
+        data = response.text
+        patid = 'href="/.*?/\d+/(\d{8})"'
+        pattitle = '"isPaid":false,"tag":0,"title":"(.*?)","playCount"'
+        mediumtitle = re.compile(pattitle).findall(data)
+        mediumid = re.compile(patid).findall(data)
+        for i in range(0,len(mediumid)):
+            if mediumid[i] not in trackId:
+                trackId.append(mediumid[i])
+        for i in range(0, len(mediumtitle)):
+            if mediumtitle[i] not in title:
+                title.append(mediumtitle[i])
+    return title,trackId
+
+def get_api(trackId):
+    api_url = 'https://www.ximalaya.com/revision/play/v1/audio?id='+trackId+'&ptype=1'
+    r = requests.get(api_url, headers=headers)
+    pat = '"src":"(.*?)",'
+    medium_url = re.compile(pat).findall(r.text)
+    return medium_url[0]
+
 if __name__ == '__main__':
-    getmessage()
+    message = getmessage()
+    for i in range(0,len(message[0])):
+        try:
+            download(message[0][i],get_api(message[1][i]))
+        except Exception as err:
+            download('错误名称'+ str(i),get_api(message[1][i]))
+            print(err)
